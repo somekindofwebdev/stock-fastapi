2a20f6f7aceba7de4a7df011e96b3a8e4e093f31
Rich Wortham <richard.wortham@pm.me>
2025-04-12 14:13:33 +0100

    Handle CORS on localhost

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,             # allows specific origins
+    allow_credentials=True,
+    allow_methods=["*"],               # allows all HTTP methods
+    allow_headers=["*"],               # allows all headers
+)
 
 @app.get("/")
 def read_root():
